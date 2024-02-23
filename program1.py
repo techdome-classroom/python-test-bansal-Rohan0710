1e2a50a85b635f97ae0ac728603794929bdf1cac
Rohan0710 <132582137+Rohan0710@users.noreply.github.com>
2024-02-23 13:30:05 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,11 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid_brack = [('{', '}'), ('(', ')'), ('[', ']')]
+        stack = []
+        for c in s:
+            if len(stack)>0 and (stack[-1], c) in valid_brack:
+                stack.pop()
+            else:
+                stack.append(c)
+        return len(stack)==0
 
 
 
